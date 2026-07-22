4dc9f22c13f61f77a259e8e4c95ecb8febea63df
vinayaksilswal <vinayaksilswal@gmail.com>
2026-07-22 16:48:47 +0530

    fix: forcefully set Prisma engineType to library in schema and env vars

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+export PRISMA_CLIENT_ENGINE_TYPE=library
+export PRISMA_CLI_QUERY_ENGINE_TYPE=library
 pip install --no-cache-dir -r requirements.txt
 prisma generate --schema=schema_py.prisma
 prisma db push --schema=schema_py.prisma
--- generate_prisma.py
+++ generate_prisma.py
@@ -33,6 +33,7 @@ python_generator = """
 generator py {
   provider             = "prisma-client-py"
   recursive_type_depth = 5
+  engineType           = "library"
 }
 """
 if "generator py" not in content:
