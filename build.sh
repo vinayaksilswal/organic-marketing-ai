009cda093cc6c5639be7317f755e437c2ab283b7
vinayaksilswal <vinayaksilswal@gmail.com>
2026-07-22 16:25:05 +0530

    chore: optimize pip install to use --no-cache-dir to prevent OOM errors during Render build

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit
 export PRISMA_BINARY_CACHE_DIR=$(pwd)/.prisma_cache
 export PRISMA_CLIENT_ENGINE_TYPE=binary
 export PRISMA_CLI_QUERY_ENGINE_TYPE=binary
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 prisma generate --schema=schema_py.prisma
 prisma db push --schema=schema_py.prisma
